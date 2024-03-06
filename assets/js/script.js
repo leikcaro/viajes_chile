debff316154336747671c5c4faa66992d0dd30b5
Leik P. Caro Berenguer <leikcaro@gmail.com>
2024-03-06 19:13:43 -0300

    añado jquery

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// tooltip necesario para poder  desplegar el mensaje en enviar
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
